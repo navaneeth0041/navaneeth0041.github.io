4e60cfe21c125d91bb725879efcbf1fd4cd2aa92
ashwinpraveengo <ashwinpraveengo@gmail.com>
2024-10-02 09:16:11 +0530

    feat:Added footer component

---
+++ hacktober-website/src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footercontents">
+          <h3>Support Open Source With</h3>
+        <div className="foss-logo">
+      <img src="/assets/amfoss_logo.png" alt="amFOSS logo" /> 
+        </div>
+        <p>We'll see you there!</p>
+        <div className="foot-icon">
+          <a href="https://google.com"><img src="/assets/hacktober_logo.png" alt="hackicon" /></a>
+          <a href="https://github.com"><img src="/assets/github.png" alt="GitHub" /></a>
+          <a href="https://google.com"><img src="/assets/channels4_profile-removebg-preview 1.png" alt="icon3" /></a>
+        </div></div>
+    </footer>
+  );
+}
+
+export default Footer;
