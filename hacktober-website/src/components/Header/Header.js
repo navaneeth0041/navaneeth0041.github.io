7dcc184d9ed956ac499cdb536d571d2ef7683108
Swayam Agrahari <swayamagrahari025@gmail.com>
2024-10-03 17:28:38 +0530

    Responsiveness in header and time component

--- hacktober-website/src/components/Header/Header.js
+++ hacktober-website/src/components/Header/Header.js
@@ -3,26 +3,25 @@ import './Header.css';
 import Countdown from "../Countdown/CountdownTimer";
 
 
-function Header(){
+function Header() {
     const eventDate = "2024-10-21";
-    return(
+    return (
         <div className="header">
             <div className="header-top">
-                <img src ='/assets/amfoss_logo.png' alt = "amfoss logo" className="logo"/>
-                <img src = '/assets/amrita_logo.png' alt="amrita logo" className="logo"/>
+                <img src='/assets/amfoss_logo.png' alt="amfoss logo" className="logo" />
+                <img src='/assets/amrita_logo.png' alt="amrita logo" className="logo" />
             </div>
             <div className="header-main">
                 <div className="left-content">
-                <img src = '/assets/hacktoberfest.png'alt="hacktoberfest logo" className="mainLogo"/>
-                <p className="subtext"> It's that time of year again.<br/>HacktoberFest is nearly upon us!</p>
-                <button className="register-btn">Register Now</button>
-
+                    <img src='/assets/hacktoberfest.png' alt="hacktoberfest logo" className="mainLogo" />
+                    <p className="subtext"> It's that time of year again.<br />HacktoberFest is nearly upon us!</p>
+                    <button className="register-btn">Register Now</button>
                 </div>
                 <div className="right-content">
-                    <Countdown eventDate={eventDate}/>
+                    <Countdown eventDate={eventDate} />
                 </div>
             </div>
-            
+
         </div>
     )
 }
