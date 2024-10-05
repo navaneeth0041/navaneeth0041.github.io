de06c601c1e0bb0cc11234b582d24cd6b7b80b97
Navaneeth B <navaneeth0041@gmail.com>
2024-10-05 20:59:57 +0530

    final changes

--- hacktober-website/src/components/Footer.js
+++ hacktober-website/src/components/Footer.js
@@ -7,13 +7,13 @@ function Footer() {
       <div className="footercontents">
         <h3>Support Open Source With</h3>
         <div className="foss-logo">
-          <img src="/assets/amfoss_logo.png" alt="fosslogo" />
+        <img src={`${process.env.PUBLIC_URL}/assets/amfoss_logo.png`} alt="amfoss logo" className="logo" />
         </div>
         <p>We'll see you there!</p>
         <div className="foot-icon">
-          <a href="https://hacktoberfest.com"><img src="/assets/hacktober_logo.png" alt="hackicon" /></a>
-          <a href="https://github.com"><img src="/assets/github.png" alt="GitHub" /></a>
-          <a href="https://digitalocean.com"><img src="/assets/channels4_profile-removebg-preview 1.png" alt="icon3" /></a>
+          <a href="https://hacktoberfest.com"><img src={`${process.env.PUBLIC_URL}/assets/hacktober_logo.png`} alt="hacktober logo"/></a>
+          <a href="https://github.com"><img src={`${process.env.PUBLIC_URL}/assets/github.png`} alt="github logo"/></a>
+          <a href="https://digitalocean.com"><img src={`${process.env.PUBLIC_URL}/assets/channels4_profile-removebg-preview 1.png`} alt="digital ocean"/></a>
         </div></div>
     </footer>
   );
