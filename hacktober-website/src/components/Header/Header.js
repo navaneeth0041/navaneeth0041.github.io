0732e405c9c6534f31123d3c1b06d0f6515792e5
Navaneeth B <navaneeth0041@gmail.com>
2024-10-05 20:53:33 +0530

    deploy

--- hacktober-website/src/components/Header/Header.js
+++ hacktober-website/src/components/Header/Header.js
@@ -19,13 +19,13 @@ function Header() {
         <div className="header">
             <div className="header-top" >
                 <a href="https://amfoss.in"><img src={`${process.env.PUBLIC_URL}/assets/amfoss_logo.png`} alt="amfoss logo" className="logo" /></a>
-                <a href="https://www.amrita.edu/campus/amritapuri/"><img src='/assets/amrita_logo.png' alt="amrita logo" className="logo" /></a>
+                <a href="https://www.amrita.edu/campus/amritapuri/"><img src={`${process.env.PUBLIC_URL}/assets/amrita_logo.png`} alt="amrita logo" className="logo" /></a>
             </div>
             <div className="header-main" >
                 <div className="box">
                 </div>
                 <div className="left-content">
-                    <img src='/assets/hacktoberfest1.png' alt="hacktoberfest logo" className="mainLogo" />
+                <img src={`${process.env.PUBLIC_URL}/assets/hacktoberfest1.png`} alt="hacktoberfest logo" className="mainLogo" />
                     <p className="subtext"> It's that time of year again.<br />HacktoberFest is nearly upon us!</p>
                     <button className="register-btn" onClick={scrollToRegistration}>Register Now</button>
                 </div>
