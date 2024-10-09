6db560d16f4e910672d6531c80ae025811550fc0
Swayam Agrahari <swayamagrahari025@gmail.com>
2024-10-10 02:34:26 +0530

    fix:fixed footer icon widths

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,13 @@ function Footer() {
       <div className="footercontents">
         <h3>Support Open Source With</h3>
         <div className="foss-logo">
-        <img src={`${process.env.PUBLIC_URL}/assets/amfoss_logo.png`} alt="amfoss logo" className="logo" />
+          <img src={`${process.env.PUBLIC_URL}/assets/amfoss_logo.png`} alt="amfoss logo" className="logo" />
         </div>
         <p>We'll see you there!</p>
         <div className="foot-icon">
-          <a href="https://hacktoberfest.com"><img src={`${process.env.PUBLIC_URL}/assets/hacktober_logo.png`} alt="hacktober logo"/></a>
-          <a href="https://github.com"><img src={`${process.env.PUBLIC_URL}/assets/github.png`} alt="github logo"/></a>
-          <a href="https://digitalocean.com"><img src={`${process.env.PUBLIC_URL}/assets/channels4_profile-removebg-preview 1.png`} alt="digital ocean"/></a>
+          <a target='_blank' href="https://hacktoberfest.com" rel="noreferrer"><img src={`${process.env.PUBLIC_URL}/assets/hacktober_logo.png`} alt="hacktober logo" /></a>
+          <a target='_blank' href="https://github.com/amfoss" rel="noreferrer"><img src={`${process.env.PUBLIC_URL}/assets/github.png`} alt="github logo" /></a>
+          <a target='_blank' href="https://digitalocean.com" rel="noreferrer"><img src={`${process.env.PUBLIC_URL}/assets/channels4_profile-removebg-preview 1.png`} alt="digital ocean" /></a>
         </div></div>
     </footer>
   );
